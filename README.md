79960a1dccacf29922924da6913cd705494a3821
cllorey <cllorey@gmail.com>
2026-08-26 11:51:17 -0700

    wednesday commits

---
+++ README.md
@@ -0,0 +1,24 @@
+# Recreating Figure 3 of Schaefer et al. (2000)
+## Purpose
+This repository contains the instructions and code to reproduce Figure 3 from the paper "Effects of hurricane disturbance on stream water concentrations and fluxes in eight tropical forest watersheds of the Luquillo Experimental Forest, Puerto Rico" by Schaefer et al. (2000)
+
+## Repository Structure
+**[This section will include information about the repository structure or file organization]**
+
+## Data Access
+The raw “Chemistry of Stream Water from the Luquillo Mountains" datasets are housed in the **data/knb-lter-luq.20.4923064** folder. The files needed to recreate this figure are as follows:  
+
+- QuebradaCuenca1-Bisley.csv  
+- QuebradaCuenca2-Bisley.csv  
+- QuebradaCuenca3-Bisley.csv  
+- RioMameyesPuenteRoto.csv
+
+
+This data is maintained online through the Environmental Data Initiative. You can find them here: [McDowell and International Institute Of Tropical Forestry (IITF) 2024](https://portal.edirepository.org/nis/mapbrowse?packageid=knb-lter-luq.20.4923064).
+
+## Authors
+Courtney Lorey
+
+## References 
+McDowell, William H., and USDA Forest Service. International Institute Of Tropical Forestry (IITF). 2024. “Chemistry of Stream Water from the Luquillo Mountains.” Environmental Data Initiative. https://doi.org/10.6073/PASTA/F31349BEBDC304F758718F4798D25458.  
+Schaefer, Douglas. A., William H. McDowell, Fredrick N. Scatena, and Clyde E. Asbury. 2000. “Effects of Hurricane Disturbance on Stream Water Concentrations and Fluxes in Eight Tropical Forest Watersheds of the Luquillo Experimental Forest, Puerto Rico.” Journal of Tropical Ecology 16 (2): 189–207. https://doi.org/10.1017/s0266467400001358.
--- scratch/spaghetti.R
+++ scratch/spaghetti.R
@@ -1,116 +1,279 @@
 library(tidyverse)
 
 bq1_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca1-Bisley.csv")
-glimpse(bq1_data)
 bq2_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca2-Bisley.csv")
-glimpse(bq2_data)
 bq3_data <- read_csv("data/knb-lter-luq.20.4923064/QuebradaCuenca3-Bisley.csv")
-glimpse(bq3_data)
 prm_data <- read_csv("data/knb-lter-luq.20.4923064/RioMameyesPuenteRoto.csv")
-glimpse(prm_data)
 
-fig3_raw <- bind_rows(bq1_data, bq2_data, bq3_data, prm_data)
-view(fig3_raw)
+## Don't know if I'll need the below, these were day 1 musings to try to get a plot
 
-fig_3_ions <- fig3_raw |> 
-  select("Sample_ID", "Sample_Date", "NO3-N", "K", "Mg", "Ca", "NH4-N")
+# fig_3_ions <- fig3_raw |> 
+#   select("Sample_ID", "Sample_Date", "NO3-N", "K", "Mg", "Ca", "NH4-N")
 
-view(fig_3_ions)
+# fig_3_long <- fig_3_ions |> 
+#   pivot_longer(
+#     cols = "NO3-N":"NH4-N",
+#     names_to = "Nutrient",
+#     values_to = "Concentration"
+#   )
 
+# ggplot(
+#     data = fig_3_long,
+#     mapping = aes(
+#         x = Sample_Date,
+#         y = Concentration,
+#         color = Sample_ID
+#     )
+# ) + 
+#   geom_line() +
+#   facet_wrap(~Nutrient, scales = "free")
 
+## DAY 2: Make a for loop for bq1 to get a moving average for just one of the data frames! 
 
-fig_3_long <- fig_3_ions |> 
-  pivot_longer(
-    cols = "NO3-N":"NH4-N",
-    names_to = "Nutrient",
-    values_to = "Concentration"
-  )
+bq1 <- bq1_data |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1996-12-31")
 
-fig_3_long
-view(fig_3_long)
-
-ggplot(
-    data = fig_3_long,
-    mapping = aes(
-        x = Sample_Date,
-        y = Concentration,
-        color = Sample_ID
-    )
-) + 
-  geom_line() +
-  facet_wrap(~Nutrient, scales = "free")
-
-
-
-# moving average attempt  ------------------------------------------------
-
-fig_3_ions
-
-fig_smoothed <- tibble(
-    window_start = seq(fig_3_ions$Sample_Date[1],
-        fig_3_ions$Sample_Date[nrow(fig_3_ions)],
+bq1_smoothed <- tibble(
+    window_start = seq(
+        bq1$Sample_Date[1],
+        bq1$Sample_Date[nrow(bq1)],
         by = "9 weeks"
     ),
+    Sample_ID = "BQ1",
     "NO3-N" = NA, 
     "K" = NA, 
     "Mg" = NA, 
     "Ca" = NA, 
     "NH4-N" = NA
 )
+bq1_smoothed
 
-fig_smoothed
-
-
-# comments from day 10 moving average -------------------------------------------
-
-# qs_smoothed <- tibble(
-#     window_start = seq(
-#         qs_data$sample_date[1], 
-#         qs_data$sample_date[nrow(qs_data)], 
-#         by = "9 days"
-#     ),
-#     k_mgl = NA,
-#     mg_mgl = NA
-# )
-# qs_smoothed
-
-# for (i in 1:nrow(qs_smoothed)) {
-#   w1 <- qs_smoothed$window_start[i]
-#   w2 <- w1+9
-#   print(w1)
-#   print(w2)
+for (i in 1:nrow(bq1_smoothed)) {
+  w1 <- bq1_smoothed$window_start[i]
+  w2 <- w1 + weeks(9)
+  print(w1)
+  print(w2)
   
-#   pot <- qs_data$k_mgl[qs_data$sample_date >= w1 & qs_data$sample_date < w2]
-#   mag <- qs_data$mg_mgl[qs_data$sample_date >= w1 & qs_data$sample_date < w2]
-#   print(pot)
-#   print(mag)
+  no3n <- bq1$"NO3-N"[bq1$Sample_Date >= w1 & bq1$Sample_Date < w2]
+  k <- bq1$K[bq1$Sample_Date >= w1 & bq1$Sample_Date < w2]
+  mg <- bq1$Mg[bq1$Sample_Date >= w1 & bq1$Sample_Date < w2]
+  ca <- bq1$Ca[bq1$Sample_Date >= w1 & bq1$Sample_Date < w2]
+  nh4n <- bq1$"NH4-N"[bq1$Sample_Date >= w1 & bq1$Sample_Date < w2]
+  print(no3n)
+  print(k)
+  print(mg)
+  print(ca)
+  print(nh4n)
   
-#   qs_smoothed$k_mgl[i] <- mean(pot, na.rm = TRUE)
-#   qs_smoothed$mg_mgl[i] <- mean(mag, na.rm = TRUE)
-# }
+  bq1_smoothed$"NO3-N"[i] <- mean(no3n, na.rm = TRUE)
+  bq1_smoothed$K[i] <- mean(k, na.rm = TRUE)
+  bq1_smoothed$Mg[i] <- mean(mg, na.rm = TRUE)
+  bq1_smoothed$Ca[i] <- mean(ca, na.rm = TRUE)
+  bq1_smoothed$"NH4-N"[i] <- mean(nh4n, na.rm = TRUE)
+}
 
-# qs_smoothed
+bq1_smoothed
+view(bq1_smoothed)
 
+bq1_smoothed_long <- bq1_smoothed |> 
+  pivot_longer(
+    cols = "NO3-N":"NH4-N",
+    names_to = "Nutrient",
+    values_to = "Concentration"
+  )
+bq1_smoothed_long
 
-# qs_long <- qs_smoothed |> 
-#   pivot_longer(
-#     cols = k_mgl:mg_mgl,
-#     names_to = "ion",
-#     values_to = "concentration"
-#   )
-# qs_long
+ggplot(
+    data = bq1_smoothed_long,
+    mapping = aes(
+        x = window_start,
+        y = Concentration
+    )
+) + 
+  geom_line() +
+  facet_wrap(~Nutrient, scales = "free") +
+  scale_x_date(name = "Years")
 
-# ggplot(
-#     data = qs_long,
-#     mapping = aes(
-#         x = window_start,
-#         y = concentration,
-#         color = ion
-#     )
-# ) + geom_point() +
-#   scale_y_continuous(
-#     name = "Average Concentration (mgl)"
-#   ) + 
-#   scale_x_date(
-#     name = "Date"
-#   )
+# bq2 workspace
+
+bq2 <- bq2_data |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1996-12-31")
+
+bq2_smoothed <- tibble(
+    window_start = seq(
+        bq2$Sample_Date[1],
+        bq2$Sample_Date[nrow(bq2)],
+        by = "9 weeks"
+    ),
+    Sample_ID = "BQ2",
+    "NO3-N" = NA, 
+    "K" = NA, 
+    "Mg" = NA, 
+    "Ca" = NA, 
+    "NH4-N" = NA
+)
+bq2_smoothed
+
+for (i in 1:nrow(bq2_smoothed)) {
+  w1 <- bq2_smoothed$window_start[i]
+  w2 <- w1 + weeks(9)
+  print(w1)
+  print(w2)
+  
+  no3n <- bq2$"NO3-N"[bq2$Sample_Date >= w1 & bq2$Sample_Date < w2]
+  k <- bq2$K[bq2$Sample_Date >= w1 & bq2$Sample_Date < w2]
+  mg <- bq2$Mg[bq2$Sample_Date >= w1 & bq2$Sample_Date < w2]
+  ca <- bq2$Ca[bq2$Sample_Date >= w1 & bq2$Sample_Date < w2]
+  nh4n <- bq2$"NH4-N"[bq2$Sample_Date >= w1 & bq2$Sample_Date < w2]
+  print(no3n)
+  print(k)
+  print(mg)
+  print(ca)
+  print(nh4n)
+  
+  bq2_smoothed$"NO3-N"[i] <- mean(no3n, na.rm = TRUE)
+  bq2_smoothed$K[i] <- mean(k, na.rm = TRUE)
+  bq2_smoothed$Mg[i] <- mean(mg, na.rm = TRUE)
+  bq2_smoothed$Ca[i] <- mean(ca, na.rm = TRUE)
+  bq2_smoothed$"NH4-N"[i] <- mean(nh4n, na.rm = TRUE)
+}
+
+bq2_smoothed
+view(bq2_smoothed)
+
+bq2_smoothed_long <- bq2_smoothed |> 
+  pivot_longer(
+    cols = "NO3-N":"NH4-N",
+    names_to = "Nutrient",
+    values_to = "Concentration"
+  )
+bq2_smoothed_long
+
+# bq3 workspace
+
+bq3 <- bq3_data |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1996-12-31")
+
+bq3_smoothed <- tibble(
+    window_start = seq(
+        bq3$Sample_Date[1],
+        bq3$Sample_Date[nrow(bq3)],
+        by = "9 weeks"
+    ),
+    Sample_ID = "BQ3",
+    "NO3-N" = NA, 
+    "K" = NA, 
+    "Mg" = NA, 
+    "Ca" = NA, 
+    "NH4-N" = NA
+)
+bq3_smoothed
+
+for (i in 1:nrow(bq3_smoothed)) {
+  w1 <- bq3_smoothed$window_start[i]
+  w2 <- w1 + weeks(9)
+  print(w1)
+  print(w2)
+  
+  no3n <- bq3$"NO3-N"[bq3$Sample_Date >= w1 & bq3$Sample_Date < w2]
+  k <- bq3$K[bq3$Sample_Date >= w1 & bq3$Sample_Date < w2]
+  mg <- bq3$Mg[bq3$Sample_Date >= w1 & bq3$Sample_Date < w2]
+  ca <- bq3$Ca[bq3$Sample_Date >= w1 & bq3$Sample_Date < w2]
+  nh4n <- bq3$"NH4-N"[bq3$Sample_Date >= w1 & bq3$Sample_Date < w2]
+  print(no3n)
+  print(k)
+  print(mg)
+  print(ca)
+  print(nh4n)
+  
+  bq3_smoothed$"NO3-N"[i] <- mean(no3n, na.rm = TRUE)
+  bq3_smoothed$K[i] <- mean(k, na.rm = TRUE)
+  bq3_smoothed$Mg[i] <- mean(mg, na.rm = TRUE)
+  bq3_smoothed$Ca[i] <- mean(ca, na.rm = TRUE)
+  bq3_smoothed$"NH4-N"[i] <- mean(nh4n, na.rm = TRUE)
+}
+
+bq3_smoothed
+view(bq3_smoothed)
+
+bq3_smoothed_long <- bq3_smoothed |> 
+  pivot_longer(
+    cols = "NO3-N":"NH4-N",
+    names_to = "Nutrient",
+    values_to = "Concentration"
+  )
+bq3_smoothed_long
+
+# PRM workspace
+
+prm <- prm_data |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1996-12-31")
+
+prm_smoothed <- tibble(
+    window_start = seq(
+        prm$Sample_Date[1],
+        prm$Sample_Date[nrow(prm)],
+        by = "9 weeks"
+    ),
+    Sample_ID = "PRM",
+    "NO3-N" = NA, 
+    "K" = NA, 
+    "Mg" = NA, 
+    "Ca" = NA, 
+    "NH4-N" = NA
+)
+prm_smoothed
+
+for (i in 1:nrow(prm_smoothed)) {
+  w1 <- prm_smoothed$window_start[i]
+  w2 <- w1 + weeks(9)
+  print(w1)
+  print(w2)
+  
+  no3n <- prm$"NO3-N"[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  k <- prm$K[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  mg <- prm$Mg[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  ca <- prm$Ca[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  nh4n <- prm$"NH4-N"[prm$Sample_Date >= w1 & prm$Sample_Date < w2]
+  print(no3n)
+  print(k)
+  print(mg)
+  print(ca)
+  print(nh4n)
+  
+  prm_smoothed$"NO3-N"[i] <- mean(no3n, na.rm = TRUE)
+  prm_smoothed$K[i] <- mean(k, na.rm = TRUE)
+  prm_smoothed$Mg[i] <- mean(mg, na.rm = TRUE)
+  prm_smoothed$Ca[i] <- mean(ca, na.rm = TRUE)
+  prm_smoothed$"NH4-N"[i] <- mean(nh4n, na.rm = TRUE)
+}
+
+prm_smoothed
+view(prm_smoothed)
+
+prm_smoothed_long <- prm_smoothed |> 
+  pivot_longer(
+    cols = "NO3-N":"NH4-N",
+    names_to = "Nutrient",
+    values_to = "Concentration"
+  )
+prm_smoothed_long
+
+## Bind & plot - bind the rows using something like: 
+# fig3_raw <- bind_rows(bq1_data, bq2_data, bq3_data, prm_data)
+
+fig3 <- bind_rows(bq1_smoothed_long, bq2_smoothed_long, bq3_smoothed_long, prm_smoothed_long)
+fig3
+view(fig3)
+
+ggplot(
+    data = fig3,
+    mapping = aes(
+        x = window_start,
+        y = Concentration,
+        color = Sample_ID
+    )
+) + 
+  geom_line() +
+  facet_wrap(~Nutrient, scales = "free", ncol = 1) +
+  scale_x_date(name = "Years")
