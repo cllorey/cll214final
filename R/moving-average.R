46ab8bca37064004da56472c1f50bd7c544095e2
cllorey <cllorey@gmail.com>
2026-08-27 16:33:33 -0700

    day 4 updates

--- R/moving-average.R
+++ R/moving-average.R
@@ -1,6 +1,6 @@
 # The input to this function should be a data frame containing stream chemistry data
 moving_average <- function(site_data) {
-  # Initialize a tibble to contain the results
+  # Tibble to contain results 
   result <- tibble(
     window_start = seq(site_data$Sample_Date[1], site_data$Sample_Date[nrow(site_data)], by = "9 weeks"),
     Site = site_data$Site[1],
@@ -11,24 +11,22 @@ moving_average <- function(site_data) {
     nh4n_ugL = NA
   )
 
-  # Fill in the iterator and sequence
   for (i in 1:nrow(result)) {
-    # Create variables for the start and end of the current window
+    # Variables for the start and end of the current window
     w1 <- result$window_start[i]
     w2 <- w1 + weeks(9)
 
-    # Create a logical vector, called "in_window", that says which samples are inside the window
-    # Hint: you'll compare sample dates to the start and end of the window
+    # Logical vector that defines which samples are inside the window
     in_window <- site_data$Sample_Date >= w1 & site_data$Sample_Date < w2
 
-    # Use indexing to pull out the ion concentrations that fall inside the window
+    # Indexes ion concentrations that fall inside the window
     no3n_window <- site_data$`NO3-N`[in_window]
     k_window <- site_data$K[in_window]
     mg_window <- site_data$Mg[in_window]
     ca_window <- site_data$Ca[in_window]
     nh4n_window <- site_data$`NH4-N`[in_window]
 
-    # Calculate the mean of each ion concentration and fill in the result
+    # Calculates the mean of each ion concentration and fill in the result
     result$no3n_ugL[i] <- mean(no3n_window, na.rm = TRUE)
     result$k_mgL[i] <- mean(k_window, na.rm = TRUE)
     result$mg_mgL[i] <- mean(mg_window, na.rm = TRUE)
